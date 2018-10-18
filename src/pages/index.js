beb6daaea7903c98464ee157b235129e843e30fd
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2018-10-18 23:33:04 +0200

    Changed index.js to the stateful component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React,{ Component } from 'react'
 import Info from './../components/Info/Info'
 import BlueBarInfo from './../components/BlueBar/BlueBarInfo/BlueBarInfo'
 import Fragment from 'render-fragment'
 import MainCarousel from './../components/MainCarousel/MainCarousel'
 
 
-const IndexPage = () => {
+class IndexPage  extends Component {
 
-  return(
-
-   <Fragment>
-     <MainCarousel/>
-     <Info/>
-     <BlueBarInfo/>
-   </Fragment>
-  )
-
+  render(){
+    return(
+      <Fragment>
+        <MainCarousel />
+        <Info />
+        <BlueBarInfo />
+      </Fragment>
+    )
+  }
 }
 
 export default IndexPage
