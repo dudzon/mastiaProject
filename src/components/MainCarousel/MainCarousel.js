2979b5fbde39a88ff20aebbb51d9be1eafd29f91
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2018-10-18 23:31:49 +0200

    Main carousel responsive

--- src/components/MainCarousel/MainCarousel.js
+++ src/components/MainCarousel/MainCarousel.js
@@ -1,27 +1,85 @@
 import React, { Component } from 'react'
 import Carousel from 'nuka-carousel';
 import classes from './MainCarousel.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 class MainCarousel extends Component {
 
-    state ={
-       height: null
-    }
+    state = {
+        slideIndex: 0,
+        clientWidth:window.innerWidth
+      };
 
-      componentDidMount() {
-        const height = Math.floor(this.divElement.getBoundingClientRect().top);
-        console.log(height)
-        this.setState({height})
-      }
     render(){
-        console.log(this.state)
-        let clientH = document.documentElement.clientHeight;
-        let imgHeight =  clientH - this.state.height;
-        console.log(clientH)
-        console.log(imgHeight)
+        let leftArrowStyle;
+        let leftButtonStyle =
+        {
+            background:'none',
+            border:'none',
+            outline:0,
+            height:60,
+            width:60,
+            marginLeft:'1.5rem',
+            cursor:'pointer'
+        }
+        let rightButtonStyle =
+        {
+            background:'none',
+            border:'none',
+            outline:0,
+            height:60,
+            width:60,
+            marginRight:'1.5rem',
+            cursor:'pointer'
+        }
+        let size = '3x';
+        if( this.state.slideIndex == 0){
+            leftArrowStyle = {color:'#ccc'}
+            leftButtonStyle = { ...leftButtonStyle,
+                            border:'1px solid #ccc'}
+        } else{
+            leftArrowStyle = {color:'#fff'}
+            leftButtonStyle = { ...leftButtonStyle,
+                border:'1px solid #fff'}
+        }
+        let rightArrowStyle;
+        if( this.state.slideIndex == 2){
+            rightArrowStyle = {color:'#ccc'}
+            rightButtonStyle = { ...rightButtonStyle,
+                border:'1px solid #ccc'}
+        } else{
+            rightArrowStyle = {color:'#fff'}
+            rightButtonStyle = { ...rightButtonStyle,
+                border:'1px solid #fff'}
+        }
+        if ( this.state.clientWidth < 576){
+            leftButtonStyle = { ...leftButtonStyle,
+                width:40,
+                height:40}
+                size='1x';
+            rightButtonStyle = { ...rightButtonStyle,
+                width:40,
+                height:40}
+                size='1x';
+        }
+
         return(
-            <Carousel className ={classes.main__carousel}  wrapAround ={true} >
-                <div className = {classes.carousel__item1 } ref={ (divElement) => this.divElement = divElement} style ={{height:imgHeight}} >
+            <Carousel className ={classes.main__carousel}
+                      slideIndex={this.state.slideIndex}
+                      afterSlide={slideIndex => this.setState({slideIndex })}
+                      renderCenterLeftControls={({ previousSlide }) => (
+                        <button onClick={previousSlide} style= {leftButtonStyle}>
+                         <FontAwesomeIcon icon={ ['fas','angle-left'] } size={size} style ={leftArrowStyle}/>
+                        </button>
+                )}
+                renderCenterRightControls={({ nextSlide }) => (
+                    <button onClick={nextSlide} style = {rightButtonStyle}>
+                    <FontAwesomeIcon icon={ ['fas','angle-right'] } size={size} style ={rightArrowStyle}/>
+                    </button>
+                )}
+                renderBottomCenterControls={() => (false)}>
+                <div className = {classes.carousel__item1 }  >
                     <div className = {classes.carousel__inner}>
                         <p className = {classes.carousel__title}>We care about your business</p>
                         <p className = {classes.carousel__text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam elementum consectetur egestas. Nulla nisi nisi, rutrum vitae nunc non, bibendum euismod nulla</p>
@@ -29,14 +87,14 @@ class MainCarousel extends Component {
                     </div>
 
                 </div>
-                <div className= {classes.carousel__item2} style ={{height:imgHeight}}>
+                <div className= {classes.carousel__item2} >
                     <div className={classes.carousel__inner}>
                         <p className={classes.carousel__title}>We make your business stronger</p>
                         <p className={classes.carousel__text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam elementum consectetur egestas. Nulla nisi nisi, rutrum vitae nunc non, bibendum euismod nulla</p>
                         <button className={classes.carousel__button}>About us</button>
                     </div>
                 </div>
-                <div className= {classes.carousel__item3} style ={{height:imgHeight}}>
+                <div className= {classes.carousel__item3}>
                     <div className={classes.carousel__inner}>
                         <p className={classes.carousel__title}>We are happy to help you</p>
                         <p className={classes.carousel__text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam elementum consectetur egestas. Nulla nisi nisi, rutrum vitae nunc non, bibendum euismod nulla</p>
