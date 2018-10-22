58489284f2d8efce474a8471ec03003ca28121d7
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2018-10-22 13:31:27 +0200

    wider Info Item component

--- src/components/Info/InfoItem/InfoItem.js
+++ src/components/Info/InfoItem/InfoItem.js
@@ -4,10 +4,7 @@ import classes from './InfoItem.module.scss'
 const InfoItem = ( props ) => {
   return (
         <div className = { classes.infoItem } style = { props.align }>
-            {/* <picture> */}
-                {/* <source media = "(max-width:768px)" srcSet = { props.mobileIcon }/> */}
-                <img src= { props.normalIcon } alt="Icon"/>
-            {/* </picture> */}
+             <img src= { props.normalIcon } alt="Icon"/>
             <h3 className = { classes.infoItem__header }> { props.title }</h3>
             <p className = { classes.infoItem__text } >Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus etmagnis dis parturie nt montes, nascetur.</p>
         </div>
