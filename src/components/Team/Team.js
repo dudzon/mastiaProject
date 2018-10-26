4532d6431af5c5618ca305db654b59735fd0d2dc
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2018-10-26 11:29:42 +0200

    Team component completed

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -1,86 +1,62 @@
-import React, { Component } from 'react'
+import React from 'react'
 import classes from './Team.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Member from './Member/Member'
 import person1 from './../../assets/person1.jpg'
 import person2 from './../../assets/person2.jpg'
 import person3 from './../../assets/person3.jpg'
 import person4 from './../../assets/person4.jpg'
-const Team = () => {
-        return (
-            <div className={classes.Team}>
-                <div className={classes.Team__wrapper}>
-                    <h3 className={classes.Team__title}>Our crazy Team</h3>
-                    <div className={classes.Team__members}>
-                        <div className={classes.Team__member}>
-                            <img src={person1} alt='' className={classes.Member__picture} />
-                            <h4 className={classes.Member__name}>Shila Khondoker</h4>
-                            <p className={classes.Member__position}>Chief technical officer</p>
-                            <div className={classes.Member__social}>
-                                <a href='https://www.facebook.com'>
-                                    <FontAwesomeIcon icon={['fab', 'facebook-f']} size='xs' className={classes.Member__icons} />
-                                </a>
-                                <a href="https://www.twitter.com">
-                                    <FontAwesomeIcon icon={['fab', 'twitter']} size='xs' className={classes.Member__icons} />
-                                </a>
-                                <a href="https://www.linkedin.com">
-                                    <FontAwesomeIcon icon={['fab', 'linkedin-in']} size='xs' className={classes.Member__icons} />
-                                </a>
-                            </div>
-                        </div>
-                        <div className={classes.Team__member}>
-                            <img src={person2} alt='' className={classes.Member__picture} />
-                            <h4 className={classes.Member__name}>Max Puttner</h4>
-                            <p className={classes.Member__position}>Chief Analyst</p>
-                            <div className={classes.Member__social}>
-                                <a href='https://www.facebook.com'>
-                                    <FontAwesomeIcon icon={['fab', 'facebook-f']} size='xs' className={classes.Member__icons} />
-                                </a>
-                                <a href="https://www.twitter.com">
-                                    <FontAwesomeIcon icon={['fab', 'twitter']} size='xs' className={classes.Member__icons} />
-                                </a>
-                                <a href="https://www.linkedin.com">
-                                    <FontAwesomeIcon icon={['fab', 'linkedin-in']} size='xs' className={classes.Member__icons} />
-                                </a>
-                            </div>
-                        </div>
-                        <div className={classes.Team__member}>
-                            <img src={person3} alt='' className={classes.Member__picture} />
-                            <h4 className={classes.Member__name}>Mark Evans</h4>
-                            <p className={classes.Member__position}>HR Specialist</p>
-                            <div className={classes.Member__social}>
-                                <a href='https://www.facebook.com'>
-                                    <FontAwesomeIcon icon={['fab', 'facebook-f']} size='xs' className={classes.Member__icons} />
-                                </a>
-                                <a href="https://www.twitter.com">
-                                    <FontAwesomeIcon icon={['fab', 'twitter']} size='xs' className={classes.Member__icons} />
-                                </a>
-                                <a href="https://www.linkedin.com">
-                                    <FontAwesomeIcon icon={['fab', 'linkedin-in']} size='xs' className={classes.Member__icons} />
-                                </a>
-                            </div>
-                        </div>
-                        <div className={classes.Team__member}>
-                            <img src={person4} alt='' className={classes.Member__picture} />
-                            <h4 className={classes.Member__name}>David Probst</h4>
-                            <p className={classes.Member__position}>Senior Frontend Engineer</p>
-                            <div className={classes.Member__social}>
-                                <a href='https://www.facebook.com'>
-                                    <FontAwesomeIcon icon={['fab', 'facebook-f']} size='xs' className={classes.Member__icons} />
-                                </a>
-                                <a href="https://www.twitter.com">
-                                    <FontAwesomeIcon icon={['fab', 'twitter']} size='xs' className={classes.Member__icons} />
-                                </a>
-                                <a href="https://www.linkedin.com">
-                                    <FontAwesomeIcon icon={['fab', 'linkedin-in']} size='xs' className={classes.Member__icons} />
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+const members = [
+        {
+            id: Math.random(),
+            picture: person1,
+            alt: 'Picture of businessman walking in a misty city',
+            name:'John McCoist',
+            position:'Chief technical officer'
+        },
+        {
+            id:Math.random(),
+            picture: person2,
+            alt: 'Picture of a young person staring at something',
+            name:'Max Puttner',
+            position:'Chief analyst'
+        },
+        {
+            id:Math.random(),
+            picture: person3,
+            alt:'Picture of a middleaged man giving a lecture',
+            name:'Mark Evans',
+            position:'HR Specialist'
+        },
+        {
+            id:Math.random(),
+            picture: person4,
+            alt:'Picture of a young man holding a camera',
+            name:'David Probst',
+            position:'Senior Frontend Engineer'
+        }
+];
 
-                </div>
-            </div>
-        )
-    }
+const Team  = () => {
+    const team = members.map( member => {
+        return <Member key = { member.id }
+                       src = { member.picture }
+                       alt = { member.alt }
+                       name = { member.name }
+                       position = { member.position }
+                />
+        })
+
+        return(
+        <div className={classes.Team}>
+          <div className={classes.Team__wrapper}>
+              <h3 className={classes.Team__title}>Our crazy Team</h3>
+              <div className={classes.Team__members}>
+                  { team }
+              </div>
+          </div>
+      </div>
+  )
+}
 
 
 export default Team
