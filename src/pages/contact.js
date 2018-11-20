9361fcb778520232c26c5015c42f4d52362d003c
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2018-11-20 13:48:16 +0100

    Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,9 +2,11 @@ import React from 'react'
 import BlueBarTop from './../components/BlueBar/BlueBarTop/BlueBarTop'
 import GoogleMap from './../components/GoogleMap/GoogleMap'
 import ClearAbsoluteHeight from './../components/ClearAbsoluteHeight/ClearAbsoluteHeight'
+import Form from './../components/Form/Form'
  const Contact = () => (
   <div>
      <BlueBarTop title = 'Contact' />
+     <Form/>
      <GoogleMap />
      <ClearAbsoluteHeight/>
   </div>
