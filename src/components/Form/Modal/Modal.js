0cfde94cff5e10ae2203f1e5bb9f96c877a4f3a4
Tomasz Dudzinski <tomasz.j.p.dudzinski@gmail.com>
2018-11-21 23:34:17 +0100

    Added responsiveness to Form Component

--- src/components/Form/Modal/Modal.js
+++ src/components/Form/Modal/Modal.js
@@ -16,10 +16,10 @@ const Modal = ( props ) => {
             <div className={ classes.Modal__wrapper }>
                 <h3 className = { classes.Modal__title}> Your message is ready.</h3>
                 <div className = { classes.Modal__content }>
-                    <p> From : {firstname} {lastname} </p>
-                    <p> Email address : { email }</p>
-                    <p>Website: { website } </p>
-                    <p> Message text : <span style ={{ fontStyle:'italic'}}>{ message }</span></p>
+                    <p> <strong>From:</strong> {firstname} {lastname} </p>
+                    <p> <strong>Email address :</strong> { email }</p>
+                    <p><strong>Website:</strong> { website } </p>
+                    <p> <strong>Message text :</strong> <span style ={{ fontStyle:'italic'}}>{ message }</span></p>
                     <div className = { classes.Modal__buttons }>
                         <button className= {classes.Modal__edit } onClick = { edit }>Edit Message</button>
                         <button className= { classes.Modal__send } onClick = { close }>Send Message</button>
